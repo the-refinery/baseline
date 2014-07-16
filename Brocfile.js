c5caedb8f9e8aa23dab4b50b671749752c5e972c
Branden Wiegand <branden@brandenwiegand.com>
2014-07-16 15:19:56 -0400

    Docs section now uses pages.

--- Brocfile.js
+++ Brocfile.js
@@ -1,10 +1,19 @@
 var pickFiles = require('broccoli-static-compiler'),
     compileSass = require('broccoli-ruby-sass'),
     mergeTrees = require('broccoli-merge-trees'),
+    HTMLPages = require('broccoli-pages').HTMLPages,
+    MarkdownPages = require('broccoli-pages').MarkdownPages,
+    HTMLPages = require('broccoli-pages').HTMLPages,
+    HBSPages = require('broccoli-pages').HBSPages,
+
+    pagesOptions = {
+      templates: './docs/templates',
+      helpers: './docs/helpers',
+      partials: './docs/templates/partials'
+    },
 
     source = 'source',
     test = 'test',
-    docs = 'docs',
     bourbon = 'bower_components/bourbon/dist',
     normalize = 'bower_components/normalize-scss',
 
@@ -14,21 +23,23 @@ var pickFiles = require('broccoli-static-compiler'),
       destDir: test
     }),
 
-    docsHtml = pickFiles(docs, {
+    docs = pickFiles('docs/content', {
       srcDir: '/',
-      files: ['**/*.html'],
-      destDir: docs
-    }),
-
-    trees = [bourbon, normalize, source],
-    sassAndLib = mergeTrees(trees, { overwrite: true }),
-
-    testCss = compileSass([sassAndLib, test], 'test.scss', test + '/styles.css', {
-      bundleExec: true
+      files: ['**/*.*'],
+      destDir: '/'
     });
 
-    docsCSS = compileSass([sassAndLib, docs], 'docs.scss', docs + '/styles.css', {
-      bundleExec: true
-    });
+    var docsHtml = HTMLPages(docs, pagesOptions);
+    docsHtml = MarkdownPages(docsHtml, pagesOptions);
+    docsHtml = HBSPages(docsHtml, pagesOptions);
+
+    var trees = [bourbon, normalize, source],
+        sassAndLib = mergeTrees(trees, { overwrite: true }),
+        testCss = compileSass([sassAndLib, test], 'test.scss', test + '/styles.css', {
+          bundleExec: true
+        }),
+        docsCSS = compileSass([sassAndLib, docs], 'docs.scss', docs + '/styles.css', {
+          bundleExec: true
+        });
 
 module.exports = mergeTrees([testCss, testHtml, docsCSS, docsHtml]);
